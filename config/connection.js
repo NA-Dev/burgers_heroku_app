33c89d7952c435200e7f45d65a90a661a7e3eca5
Nikki Aaron <31260131+NA-Dev@users.noreply.github.com>
2018-02-15 18:46:05 -0500

    added jawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,8 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: 'burgers_db'
-});
+const connection = mysql.createConnection(
+  process.env.JAWSDB_URL
+);
 
 connection.connect(function(err) {
   if (err) throw err;
